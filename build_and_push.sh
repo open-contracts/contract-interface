f50e82a604918265429c0a93807c06fe14567282
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2021-11-11 22:39:06 -0800

    Updating.

--- build_and_push.sh
+++ build_and_push.sh
@@ -6,4 +6,5 @@ rm -rf build/client-protocol
 git clone https://github.com/open-contracts/client-protocol build/client-protocol
 
 # push build to main
-git subtree push --prefix build origin gh-pages
+# git subtree push --prefix build origin gh-pages
+git push origin `git subtree split --prefix build main`:gh-pages --force
