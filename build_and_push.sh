4890d7a3e9cc523c12daeb39f55c8c754aa37c71
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2021-11-11 22:34:37 -0800

    Updating build script.

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,5 +1,9 @@
 git submodule update --recursive --remote
 yarn run build
+git rm --cached build/client-protocol
+rm -rf .git/modules/build/client-protocol
+rm -rf build/client-protocol
+git clone https://github.com/open-contracts/client-protocol build/client-protocol
 
 # push build to main
 git subtree push --prefix build origin gh-pages
