159b5d0bf6e49d4cf509e44a52846db4dac4595f
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2021-11-11 23:08:34 -0800

    Building

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,7 +2,7 @@ git submodule update --recursive --remote
 yarn run build
 git rm -r build/client-protocol
 rm -rf .git/modules/build/client-protocol
-# cp -r public/client-protocol build/client-protcol
+cp -r public/client-protocol build/client-protocol
 rm -r build/client-protocol/.git
 git add .
 git commit -m "Building"
