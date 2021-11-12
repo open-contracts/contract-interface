f3917d68f0c6caf53d0aaf58d4ee10b806dcaac3
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2021-11-11 22:43:57 -0800

    Building

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,10 +1,12 @@
 git submodule update --recursive --remote
 yarn run build
-git rm --cached build/client-protocol
+git rm -r build/client-protocol
 rm -rf .git/modules/build/client-protocol
 rm -rf build/client-protocol
+git add .
+git commit -m "Building"
 git clone https://github.com/open-contracts/client-protocol build/client-protocol
 
 # push build to main
-# git subtree push --prefix build origin gh-pages
-git push origin `git subtree split --prefix build main`:gh-pages --force
+git subtree push --prefix build origin gh-pages
+# git push origin `git subtree split --prefix build main`:gh-pages --force 
