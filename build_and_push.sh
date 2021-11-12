4f6af22bdc0c188da674e05c61b38d310a2ff723
open-contracts <90533597+open-contracts@users.noreply.github.com>
2021-11-12 11:15:27 -0800

    Update build_and_push.sh

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,4 +1,5 @@
 # run and commit build in main
+​git subtree pull --prefix build origin gh-pages
 git submodule update --recursive --remote
 yarn run build
 git rm -r build/client-protocol
@@ -6,7 +7,7 @@ rm -rf .git/modules/build/client-protocol
 cp -r public/client-protocol build/client-protocol
 rm -r build/client-protocol/.git
 git add .
-git commit -m "Building"
+git commit -m "Built Page"
 git push
 
 # push build to gh-pages
