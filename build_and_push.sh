af7a15ddce365c6a087cef695203f4da052dd90d
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2021-11-11 22:46:49 -0800

    Building

--- build_and_push.sh
+++ build_and_push.sh
@@ -3,9 +3,9 @@ yarn run build
 git rm -r build/client-protocol
 rm -rf .git/modules/build/client-protocol
 rm -rf build/client-protocol
+git clone https://github.com/open-contracts/client-protocol build/client-protocol
 git add .
 git commit -m "Building"
-git clone https://github.com/open-contracts/client-protocol build/client-protocol
 
 # push build to main
 git subtree push --prefix build origin gh-pages
