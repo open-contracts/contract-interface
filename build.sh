7c97cbcb7a2c6d83e7f1508407ff6c5b9a857ff4
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2021-11-16 08:55:45 -0800

    Built Page

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 # run and commit build in main
 git submodule update --init --recursive
 git submodule update --recursive --remote
-yarn list react-scripts
 yarn install
 yarn run build
 git rm -r build/client-protocol
 rm -rf .git/modules/build/client-protocol
-cp -r public/client-protocol build/client-protocol
+rm -rf build/client-protocol
+cp -r public/client-protocol/ build/
 rm -r build/client-protocol/.git
 git add .
 git commit -m "Built Page"
