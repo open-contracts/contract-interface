62f067de38e94cea47db2eded7a980c38a6eb7af
open-contracts <90533597+open-contracts@users.noreply.github.com>
2022-02-23 08:30:55 +0000

    Update gh-pages to output generated at 68d95d9

--- client-protocol/opencontracts.js
+++ client-protocol/opencontracts.js
@@ -186,6 +186,7 @@ async function decrypt(AESkey, json) {
 }
 
 async function enclaveSession(opencontracts, f) {
+    // TODO: check registry returns for validity
     var oracleIP = new URLSearchParams(window.location.search).get('oracleIP');
     if (oracleIP) {
         console.warn("Oracle IP override: ", oracleIP);
@@ -199,7 +200,7 @@ async function enclaveSession(opencontracts, f) {
         registryIP = await opencontracts.OPNverifier.registryDomains(0);
     }
     console.warn(`Trying to connect to registry with IP ${registryIP}.`);
-    var ws = new WebSocket("wss://test.opencontracts.io:8081/${registryIP}/");
+    var ws = new WebSocket("wss://enclaves.opencontracts.io:8081/${registryIP}/");
     var secondsPassed = 0;
     var timer = setInterval(() => {secondsPassed++; if (secondsPassed>30) {clearInterval(timer)}}, 1000);
     ws.onerror = function(event) {
@@ -240,12 +241,12 @@ async function enclaveSession(opencontracts, f) {
 async function connect(opencontracts, f, oracle) {
     const oracleIP = oracle.ip;
     console.warn("connecting to oracle with IP", oracleIP);
-    const domain = "test.opencontracts.io";
+    const domain = "enclaves.opencontracts.io";
     //   oracleIP.split('/')[0];
     //const oracleIP = oracleIP.substring(domain.length+1);
     const location = oracleIP;
-    console.warn("wss://" + domain + ":8080/" + oracleIP);
-    var ws = new WebSocket("wss://" + domain + ":8081/" + location);
+    console.warn(`wss://${domain}:8081/${location}/`);
+    var ws = new WebSocket(`wss://${domain}:8081/${location}/`);
     var ETHkey = null;
     var AESkey = null;
     var encryptedAESkey = null;
