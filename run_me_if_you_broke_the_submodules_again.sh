e9750a28579fec1322752e067dbf683ffac81b83
Jonas Metzger <metzgerj@stanford.edu>
2022-02-23 23:38:52 -0800

    fix broken submodules

---
+++ run_me_if_you_broke_the_submodules_again.sh
@@ -0,0 +1,6 @@
+# sorry for being passive aggressive
+git rm -rf --cached public/client-protocol
+rm -rf public/client-protocol
+cd public
+git submodule add https://github.com/open-contracts/client-protocol
+
