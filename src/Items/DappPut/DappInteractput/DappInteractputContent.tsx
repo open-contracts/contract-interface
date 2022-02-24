2ab877b9eb35712422a1f1c4758c6b3daa8bf009
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2022-02-24 15:05:49 -0800

    updating build.

--- src/Items/DappPut/DappInteractput/DappInteractputContent.tsx
+++ src/Items/DappPut/DappInteractput/DappInteractputContent.tsx
@@ -13,6 +13,8 @@ import {Card} from "react-bootstrap";
 import Color from "color";
 import {X} from "react-bootstrap-icons";
 import {Modal} from "react-bootstrap";
+import { useOpenContractContext } from '../../../Models';
+import { LogoB } from '../../../Glitter';
 
 export type DappInteractputContentProps = {
     dappInteractput : DappInteractputI
@@ -21,6 +23,8 @@ export type DappInteractputContentProps = {
 export const DappInteractputContent : FC<DappInteractputContentProps>  = ({
     dappInteractput
 }) =>{
+
+    const {openContract} = useOpenContractContext();
     
     const [failedWindow, setFailedWindow] = useState<boolean>(false);
     const [modal, setModal] = useState(false);
@@ -55,30 +59,79 @@ export const DappInteractputContent : FC<DappInteractputContentProps>  = ({
             href={dappInteractput.sessionUrl}>{dappInteractput.sessionUrl}</a>
                 Please enable popups and try again or click the link above.
             </p>}
-            {modal && <Modal
-            show={modal}
-            style={{
-                position : "fixed",
-                zIndex : 1000,
-                width : "95vw",
-                height : "95vh",
-                left : "2.5vw",
-                top : "2.5vh",
-                padding : "10px",
-                border : `1px solid ${Colors.lilac}` ,
-                background : Color(Colors.lilac).lighten(.5).hex(),
-            }}>
-                <div 
-                className="hover-invert-sm"
+            {modal&&<div style={{
+                    position : "fixed",
+                    height : "100vh",
+                    width : "100vw",
+                    left : 0,
+                    top : 0
+                }}>
+                <Modal
+                show={modal}
                 style={{
-                    marginLeft : "auto",
-                }}></div>
-                <iframe src={dappInteractput.sessionUrl} style={{
-                    height : "100%",
-                    width : "100%",
-                    border : "none"
-                }}/>
-            </Modal>}
+                    position : "relative",
+                    height : "100vh",
+                    width : "100vw",
+                    left : 0,
+                    top : 0
+                }}>
+                    <div style={{
+                        position : "fixed",
+                        zIndex : 1000,
+                        width : "95vw",
+                        height : "95vh",
+                        left : "2.5vw",
+                        top : "2.5vh",
+                        padding : "10px",
+                        border : `1px solid ${Colors.lilac}`,
+                        borderRadius : "5px",
+                        background : Color(Colors.lilac).lighten(.5).hex(),
+                        display : "grid",
+                        gridTemplateRows : "1fr 9fr"
+                    }}>
+                        <div style={{
+                            display : "grid",
+                            gridTemplateColumns : "9fr 1fr"
+                        }}>
+                            <div style={{
+                                display : "flex",
+                                alignItems : 'center',
+                                alignContent : "center"
+                            }}>
+                                &ensp;
+                                <LogoB logoColor={Colors.lilac} size={"50px"} label={false}/>&ensp;
+                                <div>
+                                <h3 style={{
+                                    color : Colors.lilac
+                                }}>{openContract && openContract.contractName}, {dappInteractput.contractFunction.name}</h3>
+                                <span style={{
+                                    color : Colors.Maintheme
+                                }}>Contract: </span><a href={openContract && openContract.explorerUrl(openContract.contract.address)}>{openContract && openContract.contract.address}</a>
+                                </div>
+                            </div>
+                            <div 
+                            style={{
+                                display : 'flex',
+                                flexDirection :"row-reverse",
+                            }}>
+                            
+                                    <X 
+                                    className={"hover-invert-sm hover-salmon"}
+                                    onClick={()=>setModal(false)}
+                                    style={{
+                                        marginLeft : "auto",
+                                    }}/>
+                            </div>
+                        </div>
+                            <iframe src={dappInteractput.sessionUrl} style={{
+                                height : "100%",
+                                width : "100%",
+                                border : `1px solid ${Colors.Maintheme}`,
+                                borderRadius : "5px"
+                            }}/>
+                        </div>
+                </Modal>
+            </div>}
         </div>
 
     )
