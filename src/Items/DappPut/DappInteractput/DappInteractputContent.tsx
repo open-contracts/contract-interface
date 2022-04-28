5e20b838b16d5f2491b2bea3a3570b213437efa8
Open Contracts <90533597+open-contracts@users.noreply.github.com>
2022-04-28 12:53:35 -0700

    Update DappInteractputContent.tsx

--- src/Items/DappPut/DappInteractput/DappInteractputContent.tsx
+++ src/Items/DappPut/DappInteractput/DappInteractputContent.tsx
@@ -98,7 +98,7 @@ export const DappInteractputContent : FC<DappInteractputContentProps>  = ({
                             }}>
                                 <span>&nbsp;{openContract && openContract.contractName}: {dappInteractput.contractFunction.name}</span>
                             </div>
-                            <button onClick={()=>{window.getElementById('sessionWindow').focus()}}>Keyboard</button>
+                            <button onClick={()=>{document.getElementById('sessionWindow').focus()}}>Keyboard</button>
                             <div 
                             style={{
                                 display : 'flex',
